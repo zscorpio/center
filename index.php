778aa45799d1f2506b44d7c0067f1ad2170e002c
scorpio <zsw.scorpio@gmail.com>
2011-11-21 02:45:25 +0800

    1st init

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="zh_CN" lang="zh_CN">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta name="description" content="Scorpio's Center">
+	<meta name="author" content="蝎紫">
+	<meta name="keywords" content="蝎紫,中心">
+	<title>Scorpio's Center</title>
+	<link rel="stylesheet" href="./css/style.css" type="text/css">
+	<link rel="shortcut icon" href="./img/favicon.png">
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.0/jquery.min.js"></script>	
+</head>
+<body>
+	<div id="container">
+		<div id="welcome">
+			<a href="me.php">
+				<div class="part" id="me">
+					<p>Something About Me</p>
+					<p>And What I Want To Tell You</p>
+				</div>
+			</a>
+			<a href="me.php">
+				<div class="part" id="blog">
+					<p>This Is My Blog</p>
+					<p>It Stores A Lot Of Mine</p>			
+				</div>
+			</a>
+			<a href="me.php">			
+				<div class="part" id="email">
+					<p>Contact To Me</p>
+					<p>It Is Better To E-mail To Me</p>			
+				</div>
+			</a>
+			<a href="me.php">			
+				<div class="part" id="lab">
+					<p>Public Scorpio's Lab</p>
+					<p>All My New Beta Production</p>			
+				</div>
+			</a>
+			<a href="me.php">
+				<div class="part" id="resume">
+					<p>My Resume For HR</p>
+					<p>Sorry It Is Only For HR</p>			
+				</div>
+			</a>
+			<a href="me.php">
+				<div class="part" id="search">
+					<p>Searching Me</p>
+					<p>All My Accounts On Net</p>					
+				</div>	
+			</a>		
+		</div>
+	</div>
+</body>
+</html>
